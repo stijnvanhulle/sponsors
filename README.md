0e7eefaad1e9080c458b5a4e1b5f9c269d89742e
Stijn Van Hulle <stijn@stijnvanhulle.be>
2024-09-01 13:55:32 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# My sponsors
+
+<p align="center">
+  <a href="https://github.com/sponsors/stijnvanhulle">
+    <img src="https://raw.githubusercontent.com/stijnvanhulle/sponsors/main/sponsors.svg" alt="My sponsors" />
+  </a>
+</p>
+
+```
+# [Sponsored By](https://github.com/sponsors/stijnvanhulle)
+
+<p align="center">
+  <a href="https://github.com/sponsors/stijnvanhulle">
+    <img src="https://raw.githubusercontent.com/stijnvanhulle/sponsors/main/sponsors.svg" alt="My sponsors" />
+  </a>
+</p>
+```
---
+++ sponsor.config.ts
@@ -0,0 +1,57 @@
+import { defineConfig, tierPresets } from 'sponsorkit'
+
+export default defineConfig({
+  github: {
+    login: 'stijnvanhulle',
+    type: 'user',
+  },
+  outputDir: '.',
+  width: 800,
+  formats: ['json', 'svg', 'png'],
+  sponsorsAutoMerge: true,
+  tiers: [
+    {
+      title: 'Backers',
+      preset: tierPresets.base,
+    },
+    // {
+    //   title: 'Super Generous',
+    //   monthlyDollars: 50,
+    //   preset: tierPresets.medium,
+    // },
+    // {
+    //   title: 'Bronze Sponsors',
+    //   monthlyDollars: 100,
+    //   preset: tierPresets.large,
+    // },
+    // {
+    //   title: 'Silver Sponsors',
+    //   monthlyDollars: 250,
+    //   preset: tierPresets.xl,
+    // },
+    // {
+    //   title: 'Gold Sponsors',
+    //   monthlyDollars: 500,
+    //   preset: tierPresets.xl,
+    // },
+  ],
+  // Run multiple renders with different configurations
+  renders: [
+    {
+      name: 'sponsors',
+      width: 800,
+      formats: ['svg', 'png'],
+    },
+    {
+      name: 'sponsors-wide',
+      width: 1000,
+      formats: ['svg'],
+    },
+    {
+      renderer: 'circles',
+      name: 'sponsors-circles',
+      width: 1000,
+      includePastSponsors: true,
+    },
+  ],
+})
